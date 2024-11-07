da47711dfad837004b0431f6e9e879c33403b4e7
Caleb_Poggemeyer <cpoggemeyer2004@gmail.com>
2024-11-06 18:41:05 -0600

    Updated Unit Tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -30,6 +30,46 @@ namespace GithubActionsLab
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
         }
 
+        [Test]
+        public void Add_Valid_Poggemeyer()
+        {
+            Assert.AreEqual(7, Program.Add("4", "3"));
+            Assert.AreEqual(17, Program.Add("8", "9"));
+            Assert.AreEqual(27, Program.Add("9", "18"));
+        }
+
+        [Test]
+        public void Subtract_Valid_Poggemeyer()
+        {
+            Assert.AreEqual(3, Program.Subtract("1", "2"));
+            Assert.AreEqual(5, Program.Subtract("3", "2"));
+            Assert.AreEqual(12, Program.Subtract("5", "7"));
+        }
+
+        [Test]
+        public void Multiply_Valid_Poggemeyer()
+        {
+            Assert.AreEqual(3, Program.Multiply("1", "2"));
+            Assert.AreEqual(5, Program.Multiply("3", "2"));
+            Assert.AreEqual(12, Program.Multiply("5", "7"));
+        }
+
+        [Test]
+        public void Divide_Valid_Poggemeyer()
+        {
+            Assert.AreEqual(2, Program.Divide("4", "2"));
+            Assert.AreEqual(6, Program.Divide("36", "6"));
+            Assert.AreEqual(10, Program.Divide"100", "10"));
+        }
+
+        [Test]
+        public void Power_Valid_Poggemeyer()
+        {
+            Assert.AreEqual(1, Program.Power("1", "2"));
+            Assert.AreEqual(9, Program.Power("3", "2"));
+            Assert.AreEqual(78125, Program.Power("5", "7"));
+        }
+
         // Implement 3 tests per operation, following a similar pattern as above
     }
 }
